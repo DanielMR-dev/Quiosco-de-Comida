a4e71680c898c4b3c5c82635fda3f3b2079fe420
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-18 20:50:31 -0500

    660. Iterando sobre los Productos

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,10 +1,12 @@
+import ProductCard from "@/components/products/ProductCard";
 import { prisma } from "@/src/lib/prisma";
 
+// Función encargada de obtener todos los productos
 async function getProducts(category : string) {
     const products = await prisma.product.findMany({ // Buscar todos los productos
         where: {
             category: {
-                slug: category
+                slug: category // Buscar productos por categoría
             }
         }
     });
@@ -15,10 +17,20 @@ async function getProducts(category : string) {
 export default async function OrderPage({ params } : { params : { category: string} } ) {
 
     const category = ((await params).category); // Obtiene la categoría de la URL
-    const products = await getProducts(category);
+    const products = await getProducts(category); // Obtiene los productos de la categoría
     console.log(products);
 
     return (
-        <div>OrderPage</div>
+        <>
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+                {products.map(product => (
+                    <ProductCard 
+                        key={product.id}
+                        product={product}
+                    />
+                ))}
+            </div>
+        </>
     );
 };
