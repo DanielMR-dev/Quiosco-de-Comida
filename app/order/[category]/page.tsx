88890b4881d67900366b7b0ba40d74e291a93ad2
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-18 20:42:04 -0500

    659. Detectar la Categoría actual y obtener los Productos

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,12 +1,22 @@
+import { prisma } from "@/src/lib/prisma";
 
-type CategoryProps = {
-    category: string
+async function getProducts(category : string) {
+    const products = await prisma.product.findMany({ // Buscar todos los productos
+        where: {
+            category: {
+                slug: category
+            }
+        }
+    });
+
+    return products; // Retorna un array de productos
 }
 
-export default async function OrderPage({ params } : { params:Promise<CategoryProps> }) {
+export default async function OrderPage({ params } : { params : { category: string} } ) {
 
-    const { category } =  await params
-    console.log(category);
+    const category = ((await params).category); // Obtiene la categoría de la URL
+    const products = await getProducts(category);
+    console.log(products);
 
     return (
         <div>OrderPage</div>
