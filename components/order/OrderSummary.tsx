6f4d99aca8436e5e54475ef734384d858eab1cdc
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-18 21:23:20 -0500

    664. Consumir el State de Orden

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,8 +1,19 @@
+"use client"
+import { useStore } from "@/src/store";
 
 export default function OrderSummary() {
+
+    const order = useStore((state) => state.order); // Obtener el estado de la orden
+
     return (
         <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
             <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
+
+            {order.length === 0 ? <p className="text-center my-10">El carrito está vacío</p> : (
+                <div className="mt-5">
+                    <p>Si hay algo</p>
+                </div>
+            )}
         </aside>
     );
 };
