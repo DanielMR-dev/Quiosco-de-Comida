578faa0ff0ddd610bfa9506f0f33a19616fc18e5
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-18 21:41:38 -0500

    666. Agregar articulos a la orden

--- src/store.ts
+++ src/store.ts
@@ -7,9 +7,18 @@ interface Store {
     addToOrder: (product : Product) => void; // Función para agregar un item a la orden
 };
 
-export const useStore = create<Store>(() => ({
+export const useStore = create<Store>((set) => ({
     order: [], // Iniciar las ordenes vacías
     addToOrder: (product) => {
-        console.log('Agregando...', product);
+
+        const {categoryId, image, ...data} = product; // Destructurar el producto para obtener los datos necesarios
+
+        set((state) => ({
+            order: [...state.order, {
+                ...data,
+                quantity: 1,
+                subtotal: 1 * product.price
+            }]
+        }));
     }
 }));
