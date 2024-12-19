3fbd32912a2cb15e5232701b8976b7058424b06a
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-19 09:19:51 -0500

    672. Calculando el Total a Pagar

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,10 +1,15 @@
 "use client"
 import { useStore } from "@/src/store";
 import OrderDetails from "./OrderDetails";
+import { useMemo } from "react";
+import { formatCurrency } from "@/src/utils";
 
 export default function OrderSummary() {
 
     const order = useStore((state) => state.order); // Obtener el estado de la orden
+
+    // Función para calcular el total a pagar 
+    const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order]);
 
     return (
         <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
@@ -18,6 +23,11 @@ export default function OrderSummary() {
                             item={item}
                         />
                     ))}
+
+                    <p className="text-2xl mt-20 text-center">
+                        Total a pagar: {''}
+                        <span className="font-bold">{formatCurrency(total)}</span>
+                    </p>
                 </div>
             )}
         </aside>
