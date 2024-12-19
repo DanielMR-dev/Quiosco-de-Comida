6b4d83a116957f0cc840d782b7f077cb3b2b5fd6
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-19 08:44:46 -0500

    668. Evitar Duplicados

--- src/store.ts
+++ src/store.ts
@@ -7,18 +7,29 @@ interface Store {
     addToOrder: (product : Product) => void; // Función para agregar un item a la orden
 };
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set, get) => ({
     order: [], // Iniciar las ordenes vacías
     addToOrder: (product) => {
 
         const {categoryId, image, ...data} = product; // Destructurar el producto para obtener los datos necesarios
+        let order : OrderItem[] = []
 
-        set((state) => ({
-            order: [...state.order, {
+        if(get().order.find( item => item.id === product.id )) { // Si ya existe un item en la orden con el mismo id, actualizarlo
+            order = get().order.map( item => item.id === product.id ? { // Map para actualizar el item
+                ...item, // Tomar una copia de item
+                quantity: item.quantity + 1,
+                subtotal: item.price * (item.quantity + 1)
+            } : item);
+        } else {
+            order = [...get().order, { // Agregar el producto a la orden
                 ...data,
                 quantity: 1,
                 subtotal: 1 * product.price
             }]
+        };
+
+        set(() => ({
+            order
         }));
     }
 }));
