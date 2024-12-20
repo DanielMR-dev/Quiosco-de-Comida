910777fb62b4f9c60ed1cd438a0e631474d003bc
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-20 18:36:28 -0500

    694. Creando un Paginador

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,8 +2,11 @@ import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
-async function getProducts() {
+async function getProducts(page : number, pageSize : number) {
+    const skip = (page - 1) * pageSize; // Calcular el número de productos a saltar para mostrar
     const products = await prisma.product.findMany({ // Obtener todos los productos
+        take: pageSize, // Numero de productos a mostrar
+        skip: skip, // Saltar los productos que no se van a mostrar
         include: {
             category: true,
         }
@@ -13,9 +16,13 @@ async function getProducts() {
 
 export type ProductsWithCategory = Awaited< ReturnType<typeof getProducts> >; // Dejar que TypeScript sepa que getProducts devuelve un arreglo de productos con categoría
 
-export default async function ProductsPage() {
+export default async function ProductsPage({searchParams} : {searchParams : {page: string}}) {
 
-    const products = await getProducts(); // Llamar a la función para obtener los productos
+    const page = +searchParams.page || 1; // Obtener el número de página actual desde los Params de la URL y si no existe, establecerlo en 1
+    const pageSize = 10;
+
+
+    const products = await getProducts(page, pageSize); // Llamar a la función para obtener los productos
 
     return (
         <>
