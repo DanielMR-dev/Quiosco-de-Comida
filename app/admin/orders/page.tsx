7237e90c9f7c7da7e79ee759942cc65e7e4be479
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-20 10:27:54 -0500

    686. Obtener las Ordenes Pendientes

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,9 +1,29 @@
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function OrdersPage() {
-  return (
-    <Heading>
-        Administrar Ordenes
-    </Heading>
-  )
-}
+async function getPendingOrders() {
+	const orders = await prisma.order.findMany({ // Traer todos los pedidos pendientes 
+		where : {
+			status: false
+		},
+		include : { // Incluir el contenido del producto
+			orderProducts : {
+				include : {
+					product : true
+				}
+			}
+		}
+	});
+
+	return orders;
+};
+
+export default async function OrdersPage() {
+	const orders = await getPendingOrders(); 
+	console.log(orders);
+    return (
+        <Heading>
+            Administrar Ordenes
+        </Heading>
+    );
+};
