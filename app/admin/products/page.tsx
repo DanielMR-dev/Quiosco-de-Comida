86042ea90a06df9bfafb3ce32dfb1581d070d3b6
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-20 18:22:00 -0500

    692. Listando los Productos

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,23 @@
+import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function ProductsPage() {
+async function getProducts() {
+    const products = await prisma.product.findMany(); // Obtener todos los productos
+    return products;
+};
+
+export default async function ProductsPage() {
+
+    const products = await getProducts(); // Llamar a la función para obtener los productos
+
     return (
         <>
             <Heading>Administrar Productos</Heading>
+
+            <ProductsTable 
+                products={products}
+            />
         </>
     );
 };
