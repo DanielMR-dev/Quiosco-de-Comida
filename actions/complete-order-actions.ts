e4c0222cc6dd6bf0d3d8669ff1d136e00f3503d7
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-20 11:27:02 -0500

    690. Marcar Ordenes Completas y Validacion

--- actions/complete-order-actions.ts
+++ actions/complete-order-actions.ts
@@ -1,5 +1,27 @@
 "use server"
 
-export async function completeOrder() {
-    console.log('Desde complete Order');
+import { prisma } from "@/src/lib/prisma";
+import { OrderIdSchema } from "@/src/schema";
+
+export async function completeOrder(formData : FormData) {
+    const data = {
+        orderId : formData.get('order_id') // Obtener el id del pedido
+    }
+    const result = OrderIdSchema.safeParse(data); // Validar que el id del pedido sea válido
+    if(result.success) { // Si es válido cambiar el estado de la orden
+        try {
+            await prisma.order.update({
+                where: { // Buscar la orden por id
+                    id: result.data.orderId
+                },
+                data: { // Actualizar el estado y la hora de la orden cuando está lista
+                    status: true,
+                    orderReadyAt: new Date(Date.now())
+                }
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
 };
