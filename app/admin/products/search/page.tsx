7e753030fc9d622f73b0d409ebf3ee9a2c08e9ba
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-21 11:29:28 -0500

    702. Mostrando los resultados de búsqueda

--- app/admin/products/search/page.tsx
+++ app/admin/products/search/page.tsx
@@ -1,11 +1,41 @@
+import ProductSearchForm from "@/components/products/ProductSearchForm";
+import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
+
+async function searchProducts(searchTerm: string) {
+    const products = await prisma.product.findMany({ // Buscar productos en la base de datos
+        where: {
+            name: {
+                contains: searchTerm,
+                mode: 'insensitive'
+            }
+        },
+        include: {
+            category: true
+        }
+    });
+    return products;
+};
 
 export default async function SearchPage({searchParams} : {searchParams: {search: string} }) {
-    const search = ((await searchParams).search); // Obtiene la búsqueda de la URL
-    console.log(search);
+    const products = await searchProducts(searchParams.search);
+    console.log(products);
+
     return (
         <>
-            <Heading>Resultados de búsqueda</Heading>
+            <Heading>Resultados de búsqueda: {searchParams.search}</Heading>
+
+            <div className="flex flex-col lg:flex-row lg:justify-end gap-5">
+                <ProductSearchForm />
+            </div>
+            {products.length ? (
+                <ProductsTable 
+                    products={products}
+                />
+            ) : <p className="text-center text-lg mt-20">No hay resultados</p> }
+
+            
         </>
     )
 };
