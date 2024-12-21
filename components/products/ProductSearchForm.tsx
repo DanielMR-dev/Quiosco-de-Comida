04a136a8eed19a4647aedd47b392b75283434348
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-21 10:25:44 -0500

    700. Validando el Buscador

--- components/products/ProductSearchForm.tsx
+++ components/products/ProductSearchForm.tsx
@@ -1,20 +1,42 @@
+"use client"
+import { SearchSchema } from "@/src/schema";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 export default function ProductSearchForm() {
+    const router = useRouter(); // Obtener la instancia del hook useRouter() para redirigir al usuario
+
+    const handleSearchForm = (formData: FormData) => {
+        const data = {
+            search: formData.get('search'), // Se obtiene el valor del campo search
+        };
+        const result = SearchSchema.safeParse(data); // Validar que el objeto cumpla con el schema
+        if(!result.success) { // Si no pasa la validación del Schema
+            result.error.issues.forEach(issue => {
+                toast.error(issue.message);
+            });
+            return
+        };
+        router.push(`/admin/products/search?search=${result.data.search}`); // Redireccionar a la ruta de búsqueda
+    };
+
     return (
-        <form className="flex items-center">
-            <input 
-                type="text" 
-                placeholder="Buscar Producto"
-                className="p-2 placeholder-gray-400 w-full"
-                name="search"
-            />
+        <form 
+            action={handleSearchForm}
+            className="flex items-center"
+        >
+                <input 
+                    type="text" 
+                    placeholder="Buscar Producto"
+                    className="p-3 placeholder-gray-400 rounded mr-2 w-full"
+                    name="search"
+                />
 
-            <input 
-                type="submit" 
-                className="bg-indigo-600 p-2 uppercase text-white cursor-pointer"
-                value={'Buscar'}
-            />
-
+                <input 
+                    type="submit" 
+                    className="bg-indigo-400 hover:bg-indigo-600 p-3 uppercase text-white cursor-pointer rounded transition-colors"
+                    value={'Buscar'}
+                />
         </form>
     );
 };
