647719c676090fd9fcf1bd06d0c2d67e0a00613a
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-12-21 11:59:10 -0500

    706. Composición para utilizar Server Components dentro de Client Components

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,12 +1,16 @@
 import AddProductForm from "@/components/products/AddProductForm";
+import ProductForm from "@/components/products/ProductForm";
 import Heading from "@/components/ui/Heading";
 
 export default function CreateProductPage() {
     return (
 		<>
 			<Heading>Nuevo Producto</Heading>
-
-			<AddProductForm />
+			
+			{/* Utilizando Composición para poder renderizar un Component de Servidor dentro de un Componente de Cliente */}
+			<AddProductForm>
+				<ProductForm /> {/* Componente de Servidor entro de un Componente de Cliente */}		
+			</AddProductForm>
 		
 		</>
     )
